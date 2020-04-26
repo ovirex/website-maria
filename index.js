08acd2078ec73fbb9a35ad97c8aa4ff6dc804593
Ovidio Pérez <oapm10@gmail.com>
2020-04-26 01:13:41 -0400

    Agrega todas las rutas

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ server.use(express.static("./public"));
 server.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
+server.get("/contact", (req, res) => {
+    res.sendFile(__dirname + "/views/contact.html");
+});
+
+server.get("/portafolio", (req, res) => {
+    res.sendFile(__dirname + "/views/portafolio.html");
+});
+server.get("/zines", (req, res) => {
+    res.sendFile(__dirname + "/views/zines.html");
+});
+
+server.get("*", (req, res) => {
+    res.status(404).send("Page Not Found!");
+});
 
 server.listen(3000, () => {
     console.log("Server running");
