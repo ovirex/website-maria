2d983d4c3edf6e87f838e456a0b938b3d5682516
Ovidio Pérez <oapm10@gmail.com>
2020-04-26 00:33:50 -0400

    Modifica direccion del css en archivos html y direccion de las fuentes en el css

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const server = express();
 
 server.use(express.static("./public"));
 
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
 server.listen(3000, () => {
     console.log("Server running");
 });
