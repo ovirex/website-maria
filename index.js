07ada2f28238bd7bcbd389de2614ea2819135e1f
Ovidio Pérez <oapm10@gmail.com>
2020-04-29 03:20:17 -0400

    Agrega opción para que el formulario en contacto envíe correos

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 const express = require("express");
 const server = express();
 const { PORT } = require("./config");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
 
 server.use(express.static("./public"));
+
+let transport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.USER,
+        pass: process.env.PASS,
+    },
+    tls: { rejectUnauthorized: false },
+});
 
 server.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
+
 server.get("/contact", (req, res) => {
+    res.sendFile(__dirname + "/views/contact.html");
+});
+
+server.post("/contact", (req, res) => {
+    const { email, subject, message } = req.body;
+    const messageToSend = {
+        from: "'Ovidio Perez' <oapm10@gmail.com>", // Sender address
+        to: "oapm10@gmail.com", // List of recipients
+        subject: subject, // Subject line
+        text: message, // Plain text body
+    };
+    transport.sendMail(messageToSend, function (err, info) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(info);
+        }
+    });
     res.sendFile(__dirname + "/views/contact.html");
 });
 
