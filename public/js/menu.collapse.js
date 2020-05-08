9d80aabdb748797fc4d5eefee0152aa2c0827a3a
Ovidio Pérez <oapm10@gmail.com>
2020-05-07 21:56:09 -0400

    Crea y agrega header desplegable para pantallas pequeñas

---
+++ public/js/menu.collapse.js
@@ -0,0 +1,18 @@
+//document.addEventListener("DOMContentLoaded", function () {
+let checkbox = document.getElementById("menu");
+checkbox.addEventListener("change", () => {
+    let menuContent = document.getElementsByClassName("menu-content")[0];
+    let menuCollapse = document.getElementsByClassName("menu-collapse")[0];
+    let icon = document.getElementById("icon");
+
+    if (checkbox.checked === true) {
+        menuContent.style.display = "block";
+        menuCollapse.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
+        icon.style.background = "url(/css/cancel.svg) no-repeat";
+    } else {
+        menuContent.style.display = "none";
+        menuCollapse.style.backgroundColor = "rgba(0, 0, 0, 0)";
+        icon.style.background = "url(/css/menu.svg) no-repeat";
+    }
+});
+//});
