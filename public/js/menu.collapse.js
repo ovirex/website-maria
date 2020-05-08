ac931fedea097b3156a5f1fbf3927d8f447a4200
Ovidio Pérez <oapm10@gmail.com>
2020-05-07 23:35:17 -0400

    Arregla height del menu desplegable

--- public/js/menu.collapse.js
+++ public/js/menu.collapse.js
@@ -9,11 +9,12 @@ checkbox.addEventListener("change", () => {
         menuContent.style.display = "block";
         menuCollapse.style.backgroundColor = "rgba(0, 0, 0, 0.9)";
         icon.style.background = "url(/css/cancel.svg) no-repeat";
-        menuCollapse.style.height = "auto";
+        menuCollapse.style.height = "100vh";
     } else {
         menuContent.style.display = "none";
         menuCollapse.style.backgroundColor = "rgba(0, 0, 0, 0)";
         icon.style.background = "url(/css/menu.svg) no-repeat";
+        menuCollapse.style.height = "25%";
     }
 });
 //});
